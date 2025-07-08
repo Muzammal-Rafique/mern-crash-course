d8f2b9e7c15b8afd914e4ca8d29ab2e9a8baff62
Muzammal Rafique <muzammalrafique26@gmail.com>
2025-07-08 14:06:37 +0500

    add functionality complete task can be clear

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -23,10 +23,25 @@ router.put('/:id', async (req, res) => {
   res.json(updated);
 });
 
-// Delete Task
-router.delete('/:id', async (req, res) => {
-  await Task.findByIdAndDelete(req.params.id);
-  res.json({ message: 'Task deleted Successfully' });
+// Delete all completed tasks - must come BEFORE '/:id'
+router.delete('/clear-completed', async (req, res) => {
+  try {
+    await Task.deleteMany({ completed: true });
+    res.json({ message: 'All completed tasks deleted' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
+
+// DELETE single task by ID
+router.delete('/:id', async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Task deleted' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
 
 module.exports = router;
